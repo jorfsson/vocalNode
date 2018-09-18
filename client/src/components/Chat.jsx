0ac99dde66695a5d5b3765929dadbe2dc2f70146
Jeffrey Chea <jeffreychea234@gmail.com>
2018-09-18 18:18:21 -0500

    update options for https

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000')
+const socket = io('https://localhost:8443')
 
 class Chat extends React.Component {
   constructor(props) {
