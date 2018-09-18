eb32c3bca795c9176c25624df2273195b754fa6e
Jeffrey Chea <jeffreychea234@gmail.com>
2018-09-17 22:12:07 -0500

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const BUILD_DIR = path.resolve(__dirname, 'client/dist');
 const APP_DIR = path.resolve(__dirname, 'client/src');
 
 const config = {
-  entry: [`${APP_DIR}/Index.jsx`],
+  entry: [`${APP_DIR}/index.jsx`],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js',
